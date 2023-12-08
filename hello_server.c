8d5b43ff63519d1c95ede944ce1874282cd4f568
ArticSultan <141678081+ArticSultan@users.noreply.github.com>
2023-12-08 11:33:44 -0800

    Add files via upload

---
+++ hello_server.c
@@ -0,0 +1,63 @@
+#if defined(_WIN32)
+#ifndef _WIN32_WINNT
+#define _WIN32_WINNT 0x0600
+#endif
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#pragma comment(lib, "ws2_32.lib")
+
+#else
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <unistd.h>
+#include <errno.h>
+
+#endif
+
+
+#if defined(_WIN32)
+#define ISVALIDSOCKET(s) ((s) != INVALID_SOCKET)
+#define CLOSESOCKET(s) closesocket(s)
+#define GETSOCKETERRNO() (WSAGetLastError())
+
+#else
+#define ISVALIDSOCKET(s) ((s) >= 0)
+#define CLOSESOCKET(s) close(s)
+#define SOCKET int
+#define GETSOCKETERRNO() (errno)
+#endif
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+
+int main() {
+
+    // Configure local address
+    printf("Configuring local address...\n");
+    struct addrinfo hints;
+    memset();
+    hints.ai_family;
+    hints.ai_socktype;
+    hints.ai_flags;
+
+    // Create socket
+
+    // Bind socket to local address
+
+    // listen for a connection
+
+    // wait for connection
+
+    // Read Request
+
+    // Send Response
+    printf("Hello, World!\n");
+
+    return 0;
+
+}
